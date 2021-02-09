8e51af6ea48ed80ee62e1fd716bd506741b5e829
Ricky Bennett <norman.bennett525@gmail.com>
2021-02-09 08:11:02 -0800

    Guessing Game Day 1

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("Please enter your guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                    if(guess != secretWord)
+                    {
+                        Console.WriteLine("Your guess was wrong. Please press enter to try again. (Hint: it's an animal from Africa.)");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }    
+            }
+            if(outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("YOU WON THE GAME");
+            }
+            
+        }
+    }
+}
