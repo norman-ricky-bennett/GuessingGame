93f43cddd76edcea207f63927fb9d1161fe71308
Ricky Bennett <norman.bennett525@gmail.com>
2021-02-10 05:05:18 -0800

    updated

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,71 +10,7 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            string secretWord = "24 miles per hour";
-            bool outOfGuesses = false;
-            int guessCount = 0;
-
-
-            Console.WriteLine("Press any key to start game...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("Please enter your name...");
-            string playerName = Console.ReadLine();
-            Console.Clear();
-
-            Console.WriteLine($"Hello {playerName}, I want to play a game. Press enter to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("Welcome to SAW's Guessing Game! The ultimate battle of the brains and battle for your life!!\n" +
-                "Press ENTER to continue.....");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.WriteLine("How to Play:\n" +
-                "You have three chances to answer the question correctly.\n" +
-                "Get the question wrong three times and...\n" +
-                "Billy won't be too happy. Best of Luck!\n" +
-                "LET'S PLAY\n" +
-                "Press enter to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            Console.Write("What is the air speed velocity of an unladen swallow?");
-            string guess = Console.ReadLine();
-            
-
-            while (guess != secretWord && !outOfGuesses)
-            {
-                // adds +1 to guessCount 0
-                guessCount++;
-                Console.Clear();
-                if (guessCount == 1)
-                {
-                    Console.WriteLine("Your guess was wrong. Please press enter to try again. (Hint: It's a European swallow.)");
-                    Console.ReadLine();
-                }
-                if (guessCount == 2)
-                {
-                    Console.WriteLine("Your guess was wrong. PLease press enter to try again. (Hint: it's in miles per hour)");
-                    Console.ReadKey();
-                }
-                if (guessCount == 3)
-                {
-                    outOfGuesses = true;
-                }
-            }
-            if (outOfGuesses)
-            {
-                Console.WriteLine("You lost the game!!");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("YOU WIN!");
-                Console.ReadKey();
-            }
+            Application.Run();
         }  
     }
 }
